98ace46fe99c5e73c5f1e4d3016bff2a81ae0ce0
Muhammad Nasir Uddin Khan Shawon <cupid.srk786@gmail.com>
2023-09-09 17:24:47 +0600

    Back to top button added

---
+++ src/components/BackToTop.vue
@@ -0,0 +1,40 @@
+<script>
+export default {
+  data() {
+    return {
+      showButton: false,
+    };
+  },
+  mounted() {
+    window.addEventListener('scroll', this.handleScroll);
+  },
+  beforeDestroy() {
+    window.removeEventListener('scroll', this.handleScroll);
+  },
+  methods: {
+    handleScroll() {
+      this.showButton = window.scrollY > 100;
+    },
+    scrollToTop() {
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth',
+      });
+    },
+  },
+};
+</script>
+
+<template>
+  <button id="top" @click="scrollToTop" type="button" class="fixed text-white bg-[crimson] rounded-md bottom-4 right-4 w-8 h-8" :style="{ display: showButton ? 'block' : 'none' }" >
+    <fa-icon :icon="['fas', 'angle-up']" />
+  </button>
+</template>
+  
+<style scoped>
+#top {
+  display: none;
+  z-index: 50;
+}
+</style>
+  
--- src/layouts/AppLayout.vue
+++ src/layouts/AppLayout.vue
@@ -1,12 +1,14 @@
 <script setup>
 import { sidebarWidth } from '@/state/sidebar';
 import Sidebar from '@/includes/Sidebar.vue';
+import BackToTop from '@/components/BackToTop.vue';
 
 </script>
 
 <template>
   <Sidebar />
   <main class="" :style="{ marginLeft: sidebarWidth }">
+    <BackToTop />
     <slot />
   </main>
 </template>
